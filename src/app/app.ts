40bd10bbbf6a885ebed5cdf1377521832f8a9711
yousef <yyoousef1254@gmail.com>
2026-03-19 03:54:22 +0200

    ..

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { RegisterationPage } from "../components/registeration-page/registeration-page";
 import { Header } from "../components/header/header";
 import { Maintitle } from "../components/maintitle/maintitle";
+import { SignUp } from '../components/sign-up/sign-up';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterationPage, Header, Maintitle],
+  imports: [RouterOutlet, RegisterationPage, Header, Maintitle,SignUp],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
