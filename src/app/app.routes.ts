a8eda7615a8df242c66b8de637d9d48286845e54
moataz3mad <mo3taz3mad@gmail.com>
2026-03-19 16:14:40 +0200

    routing updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { SignInComponent } from '../components/sign-in-page/sign-in-page';
 import { RegisterationPage } from '../components/registeration-page/registeration-page';
 import { HomePage } from '../components/home-page/home-page';
+import { SignUp } from '../components/sign-up/sign-up';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: SignInComponent },
   { path: 'home', component: HomePage },
+  {path: 'sign-up', component: SignUp},
   { path: '**', redirectTo: '/sign-in' }
 ];
--- src/components/sign-up/sign-up.ts
+++ src/components/sign-up/sign-up.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
