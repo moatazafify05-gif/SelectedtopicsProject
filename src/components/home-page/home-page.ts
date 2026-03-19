9ffd6d1c1710455220ae27f06b76fa333fa928df
moataz3mad <mo3taz3mad@gmail.com>
2026-03-19 15:55:42 +0200

    ..

--- src/components/home-page/home-page.ts
+++ src/components/home-page/home-page.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import {Header } from '../header/header';
 import { Maintitle } from "../maintitle/maintitle";
 import { RegisterationPage } from "../registeration-page/registeration-page";
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-home-page',
-  imports: [Header, RouterModule, Maintitle, RegisterationPage],
+  imports: [Header, RouterModule, Maintitle, RegisterationPage, Footer],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
